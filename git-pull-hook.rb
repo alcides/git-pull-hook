2bfac9d526b76e4753c4c854c63aa163d8f63429
Alcides Fonseca <me@alcidesfonseca.com>
2008-12-30 13:08:10 +0000

    Refactored some code

--- git-pull-hook.rb
+++ git-pull-hook.rb
@@ -5,16 +5,23 @@ require 'yaml'
 
 config =  YAML::load_file('repos.yml')
 
+
+def get_local_path remote
+  config['repo_home'] + config['repos'][remote]
+end
+
 post '/' do
   push = JSON.parse(params[:payload])
   remote_repo = push['repository']['name']
-  local_repo = config['repo_home'] + config['repos'][remote_repo]
+  local_repo = get_local_path remote_repo
   `cd #{local_repo} && git pull origin master`
   "ok"
 end
 
+# GET /remote-repo used for debugging
+
 get '/:remote' do
-  remote_repo = params[:remote]
-  local_repo = config['repo_home'] + config['repos'][remote_repo]
+  remote_repo = push['repository']['name']
+  local_repo = get_local_path remote_repo
   `cd #{local_repo} && git pull origin master`
 end
