a29ee912836a81fc738f0a5245b3e81af6db6daf
andhapp <anuj@andhapp.com>
2009-04-13 04:08:59 +0800

    Added brackets to the method headers(matter of taste) and sanitised the require at the top
    Signed-off-by: Alcides Fonseca <me@alcidesfonseca.com>

--- git-pull-hook.rb
+++ git-pull-hook.rb
@@ -1,27 +1,22 @@
-require 'rubygems'
-require 'sinatra'
-require 'json'
-require 'yaml'
+%w{rubygems sinatra json yaml}.each{|x| require x}
 
 config =  YAML::load_file('repos.yml')
 
-
-def get_local_path remote
+def get_local_path(remote)
   config['repo_home'] + config['repos'][remote]
 end
 
 post '/' do
   push = JSON.parse(params[:payload])
   remote_repo = push['repository']['name']
-  local_repo = get_local_path remote_repo
+  local_repo = get_local_path(remote_repo)
   `cd #{local_repo} && git pull origin master`
   "ok"
 end
 
 # GET /remote-repo used for debugging
-
 get '/:remote' do
   remote_repo = push['repository']['name']
-  local_repo = get_local_path remote_repo
+  local_repo = get_local_path(remote_repo)
   `cd #{local_repo} && git pull origin master`
 end
